fdcb0b86c5bc2e5d8828423ed693055b2e0e2a06
Baik Gwangin <gwangin1999@naver.com>
2023-12-24 00:26:14 +0900

    feat: presigned URL 발급 로직 작성

--- src/app/api/presigned/route.ts
+++ src/app/api/presigned/route.ts
@@ -1,40 +1,45 @@
 import { NextResponse } from 'next/server';
-
 import {
-    DeleteObjectCommand,
-    GetObjectCommand,
     PutObjectCommand,
     S3Client,
 } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 
+const awsAccessKey = process.env.MY_AWS_ACCESS_KEY;
+const awsSecretKey = process.env.MY_AWS_SECRET_KEY;
+const awsS3Bucket = process.env.MY_AWS_S3_BUCKET;
+
 const storage = new S3Client({
     region: 'ap-northeast-2',
+    credentials: {
+        accessKeyId: awsAccessKey || '',
+        secretAccessKey: awsSecretKey || '',
+    },
 });
 
-export async function POST(request: Request) {
+export async function GET(request: Request) {
     const { searchParams } = new URL(request.url);
-    const fileName = searchParams.get('fileName');
+    const fileKey = searchParams.get('fileKey');
 
-    if (!fileName) {
+    if (!fileKey) {
         return NextResponse.json(
             {
-                message: '전송하려는 파일의 이름을 요청에 동봉해주세요.',
+                message: '전송하려는 fileKey 를 요청에 동봉해주세요.',
             },
             { status: 400 },
         );
     }
 
     const command: PutObjectCommand = new PutObjectCommand({
-        Bucket: process.env.AWS_S3_BUCKET_NAME || '',
-        Key: fileName,
+        Bucket: awsS3Bucket || '',
+        Key: fileKey,
     });
 
     try {
         const presignedUrl = await getSignedUrl(storage, command, {
             expiresIn: 3600,
         });
-    
+
         return NextResponse.json(
             { presignedUrl },
             {
