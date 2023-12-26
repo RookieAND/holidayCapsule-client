b3cae21e132935e8f6e78f68e5b6315a304cbfdc
RookieAND <gwangin1999@naver.com>
2023-12-27 00:44:20 +0900

    feat: 추가 개발 컴포넌트 페이지 반영

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,13 @@ import { motion } from 'framer-motion';
 
 import HolidayStarLogoSvg from '#/assets/icons/holidayStarLogo.svg';
 import EmptyCard from '#/features/home/EmptyCard';
+import KakaoButton from '#/features/home/KakaoButton';
+import TipSection from '#/features/home/TipSection';
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center bg-red-500">
-            <HolidayStarLogoSvg className="text-creme pt-24" width={280} />
+        <main className="flex min-h-screen flex-col items-center bg-red-500 py-24">
+            <HolidayStarLogoSvg className="text-creme" width={280} />
             <div className="w-full overflow-hidden">
                 <motion.section
                     initial={{ x: -160 }}
@@ -19,9 +21,11 @@ export default function Home() {
                     <EmptyCard />
                 </motion.section>
             </div>
-            <div className="w-64 bg-creme px-4 py-2 shadow-md shadow-red-700 before:content-[''] before:block before:w-4 before:h-4">
+            <div className="w-64 bg-creme px-4 py-2 shadow-md shadow-red-700 before:content-[''] before:inline before:border-t-creme before:border-l-transparent before:border-t-4 before:border-l-4 mb-auto">
                 <h3 className="text-center text-red-600">HAPPY HOLIDAY!</h3>
             </div>
+            {/* <KakaoButton /> */}
+            <TipSection/>
         </main>
     );
 }
--- src/features/home/EmptyCard.tsx
+++ src/features/home/EmptyCard.tsx
@@ -5,7 +5,7 @@ const EmptyCard = () => (
     <div className="flex flex-col gap-y-8 w-64 px-4 pt-10 py-6 items-center justify-between bg-creme text-red-500 shadow-md shadow-red-700">
         <QuestionMarkSvg className="text-red-600" width={152}/>
         <div id="divider" className="h-0.5 w-full border border-red-500" />
-        <HolidayLogoSvg className="text-red-600" width={200} />
+        <HolidayLogoSvg className="text-red-600" width={186} />
     </div>
 );1
 
